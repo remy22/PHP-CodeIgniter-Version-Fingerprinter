ee546d55c11d4a8d99e27e846459f0bb90bce280
Kevin Morssink <contact@kevinmorssink.nl>
2015-11-06 18:49:02 +0100

    test

--- tests/CiVersionTesterTest.php
+++ tests/CiVersionTesterTest.php
@@ -1,19 +1,13 @@
 <?php
 
-require __DIR__ . '/../src/autoload.php';
+//require __DIR__ . '/../src/autoload.php';
+require __DIR__ . '/../src/CiVersionTester.class.php';
 
 class CiVersionTesterTest extends PHPUnit_Framework_TestCase
 {
-    public function testPushAndPop()
+    public function test()
     {
-        $stack = array();
-        $this->assertEquals(0, count($stack));
-
-        array_push($stack, 'foo');
-        $this->assertEquals('foo', $stack[count($stack) - 1]);
-        $this->assertEquals(1, count($stack));
-
-        $this->assertEquals('foo', array_pop($stack));
-        $this->assertEquals(0, count($stack));
+        $CiVersionTests = new CiVersionTests;
+        $this->assertTrue(TRUE);
     }
 }

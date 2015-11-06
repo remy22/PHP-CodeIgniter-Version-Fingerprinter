bc2dd68845c24de58dba8edec95fce13044955ee
Kevin Morssink <contact@kevinmorssink.nl>
2015-11-06 19:00:42 +0100

    test

--- tests/CiVersionTesterTest.php
+++ tests/CiVersionTesterTest.php
@@ -5,7 +5,14 @@ class CiVersionTesterTest extends PHPUnit_Framework_TestCase
 {
     public function test()
     {
+        $version = '1';
         $CiVersionTests = new CiVersionTester\CiVersionTests;
-        $this->assertTrue(TRUE);
+        $CiVersionTests->test('controllersIndexHtml', $version);
+        $CiVersionTests->test('userGuideVersion', $version);
+        $CiVersionTests->test('applicationFolder', $version);
+        $CiVersionTests->test('librariesCalendar', $version);
+        $CiVersionTests->test('modelsIndexHtml', $version);
+        $CiVersionTests->test('licenseTxt', $version);
+        $CiVersionTests->test('systemInitUnitTest', $version);
     }
 }

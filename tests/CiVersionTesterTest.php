188fbadab5145b5bb31556e0615ea0ae7d643476
Kevin Morssink <contact@kevinmorssink.nl>
2015-11-06 22:09:11 +0100

    better unittests

--- tests/CiVersionTesterTest.php
+++ tests/CiVersionTesterTest.php
@@ -3,10 +3,28 @@ require __DIR__ . '/../src/CiVersionTester.class.php';
 
 class CiVersionTesterTest extends PHPUnit_Framework_TestCase
 {
+    public function testEmpty()
+    {
+        $this->assertFalse(new CiVersionTester\CiVersionTests()); 
+    }
+    
+    public function testFnq()
+    {
+        $version = '3.0.3';
+        $ciVersionTests = new CiVersionTester\CiVersionTests('http://www.codeigniter.com/');
+        $ciVersionTests->test('controllersIndexHtml', $version);
+        $ciVersionTests->test('userGuideVersion', $version);
+        $ciVersionTests->test('applicationFolder', $version);
+        $ciVersionTests->test('librariesCalendar', $version);
+        $ciVersionTests->test('modelsIndexHtml', $version);
+        $ciVersionTests->test('licenseTxt', $version);
+        $ciVersionTests->test('systemInitUnitTest', $version);
+    }
+
     public function testHairstreet()
     {
         $version = '2.2.0';
-        $ciVersionTests = new CiVersionTester\CiVersionTests('http://www.hairstreet.com/'); #2.2.0
+        $ciVersionTests = new CiVersionTester\CiVersionTests('http://www.hairstreet.com/');
         $ciVersionTests->test('controllersIndexHtml', $version);
         $ciVersionTests->test('userGuideVersion', $version);
         $ciVersionTests->test('applicationFolder', $version);
@@ -19,7 +37,7 @@ class CiVersionTesterTest extends PHPUnit_Framework_TestCase
     public function testVesta()
     {
         $version = '2.1.4';
-        $ciVersionTests = new CiVersionTester\CiVersionTests('https://www.vestavuurwerk.nl'); #2.1.4
+        $ciVersionTests = new CiVersionTester\CiVersionTests('https://www.vestavuurwerk.nl');
         $ciVersionTests->test('controllersIndexHtml', $version);
         $ciVersionTests->test('userGuideVersion', $version);
         $ciVersionTests->test('applicationFolder', $version);
@@ -32,7 +50,7 @@ class CiVersionTesterTest extends PHPUnit_Framework_TestCase
     public function testGraviton()
     {
         $version = '1.7.2';
-        $ciVersionTests = new CiVersionTester\CiVersionTests('http://gravitonbouldergym.nl/gdb/'); #1.7.2
+        $ciVersionTests = new CiVersionTester\CiVersionTests('http://gravitonbouldergym.nl/gdb/');
         $ciVersionTests->test('controllersIndexHtml', $version);
         $ciVersionTests->test('userGuideVersion', $version);
         $ciVersionTests->test('applicationFolder', $version);
@@ -45,7 +63,7 @@ class CiVersionTesterTest extends PHPUnit_Framework_TestCase
     public function testFnq()
     {
         $version = '1.5.3';
-        $ciVersionTests = new CiVersionTester\CiVersionTests('https://www.fnqapartments.com/res/'); #1.5.3
+        $ciVersionTests = new CiVersionTester\CiVersionTests('https://www.fnqapartments.com/res/');
         $ciVersionTests->test('controllersIndexHtml', $version);
         $ciVersionTests->test('userGuideVersion', $version);
         $ciVersionTests->test('applicationFolder', $version);

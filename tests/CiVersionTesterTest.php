79dd6babae201cd2f5a30ee1547370ae46e7f469
Kevin Morssink <contact@kevinmorssink.nl>
2015-11-06 18:57:33 +0100

    test

--- tests/CiVersionTesterTest.php
+++ tests/CiVersionTesterTest.php
@@ -1,13 +1,11 @@
-<?php namespace CiVersionTester;
-
-require_once 'PHPUnit/Autoload.php';
+<?php
 require __DIR__ . '/../src/CiVersionTester.class.php';
 
 class CiVersionTesterTest extends PHPUnit_Framework_TestCase
 {
     public function test()
     {
-        $CiVersionTests = new CiVersionTests;
+        $CiVersionTests = new CiVersionTester\CiVersionTests;
         $this->assertTrue(TRUE);
     }
 }
